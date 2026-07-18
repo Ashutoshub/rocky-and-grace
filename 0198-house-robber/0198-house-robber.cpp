60a46b339207eddf27826d35b78e59d66004c2ab
Ashutosh Kumar Sandilya <145035368+Ashutoshub@users.noreply.github.com>
2026-07-18 23:43:43 +0530

    Time: 0 ms (100%), Space: 9.9 MB (97.61%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.empty()){
+            return 0;
+        }
+        int prev2 = 0;
+        int prev1 = 0;
+        for(int i =0; i < nums.size(); i++){
+            int curr = max(prev2 + nums[i], prev1);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
