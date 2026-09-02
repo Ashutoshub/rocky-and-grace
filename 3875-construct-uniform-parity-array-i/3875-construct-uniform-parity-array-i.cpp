bd98328733a83955e4cd9a07407708c99ba1938a
Ashutosh Kumar Sandilya <145035368+Ashutoshub@users.noreply.github.com>
2026-09-02 17:08:30 +0530

    Time: 0 ms (100%), Space: 30.2 MB (67.8%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // Since it is always mathematically possible to make the array 
+        // uniformly odd (if an odd exists) or uniformly even (if no odds exist),
+        // we can simply return true in all cases.
+        return true;
+    }
+};
